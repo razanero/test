77fcaaf63a58ce39d10febaf4dd0d43b8d9b5aba
rickykaiser <rickykaiser@gmail.com>
2018-01-04 17:34:05 -0500

    inicio

---
+++ custom-addons/account_fe/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Invoice FE',
+    'version': '1.0',
+    'category': 'Accounting',
+    'sequence': 15,
+    'summary': 'Facturación Electronica',
+    'description': '',
+    'depends': ['account'],
+    'data': [],
+    'application': False,
+}
