225a2209c06bf07df143d1305db9204ca909db12
rickykaiser <rickykaiser@gmail.com>
2018-01-05 09:16:24 -0500

    inicio

--- custom-addons/account_fe/__init__.py
+++ custom-addons/account_fe/__init__.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, models
+import logging
+
+
+
+_logger = logging.getLogger(__name__)
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_invoice_open(self):
+
+        res = super(AccountInvoice, self).action_invoice_open()
+        _logger.info('Tax Base Amount not computable probably due to a change in an underlying tax (%s).')
+
+        return res
+
