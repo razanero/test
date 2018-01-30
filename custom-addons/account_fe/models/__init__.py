a0a8b8e50636a17bac1c95d0c4c218c80d9cb3c8
rickykaiser <rickykaiser@gmail.com>
2018-01-30 13:36:35 -0500

    inicio

--- custom-addons/account_fe/models/__init__.py
+++ custom-addons/account_fe/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import account_invoice
-from . import account_control
+from . import account_invoice_control
 from . import sale_advance_payment_inv_fe
--- custom-addons/account_fe/models/sale_advance_payment_inv_fe.py
+++ custom-addons/account_fe/models/sale_advance_payment_inv_fe.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import time
 
-from odoo import api, fields, models, _
+from odoo import api, exceptions, fields, models, _
+
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _name = "sale.advance.payment.inv.fe"
